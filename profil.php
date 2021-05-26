bb906d9d4c59186ae5245531903f1b0abca079bb
Antoine <82728940+AntoineLavigne@users.noreply.github.com>
2021-05-26 15:44:05 +0200

    Update profil.php

--- profil.php
+++ profil.php
@@ -50,14 +50,16 @@ session_start();
             foreach ($result_emprunteur as $row) {
             ?>
 
-                <FORM method="POST" action="" id='form'>
+                <FORM method="POST" action="modifier_profil.php" id='form'>
                     <TABLE NOBOARDER>
                         <TR>
                             <TD>
                                 <label for="nom">Prénom :</label>
                             </TD>
                             <TD>
-                                <?php echo $row['PrenomE']; ?>
+                                <div class="col-sm-10">
+                                    <input type="text" readonly class="form-control-plaintext" name="PrenomE" value="<?php echo $row['PrenomE']; ?>">
+                                </div>
                             </TD>
                         </TR>
                         <TR>
@@ -65,7 +67,9 @@ session_start();
                                 <label for="nom">Nom :</label>
                             </TD>
                             <TD>
-                                <?php echo $row['NomE']; ?>
+                                <div class="col-sm-10">
+                                    <input type="text" readonly class="form-control-plaintext" name="NomE" value="<?php echo $row['NomE']; ?>">
+                                </div>
                             </TD>
                         </TR>
                         <TR>
@@ -73,7 +77,9 @@ session_start();
                                 <label for="nom">Email :</label>
                             </TD>
                             <TD>
-                                <?php echo $row['EmailE']; ?>
+                                <div class="col-sm-10">
+                                    <input type="text" readonly class="form-control-plaintext" name="EmailE" value="<?php echo $row['EmailE']; ?>">
+                                </div>
                             </TD>
                         </TR>
                         <TR>
@@ -81,7 +87,9 @@ session_start();
                                 <label for="nom">Adresse :</label>
                             </TD>
                             <TD>
-                                <?php echo $row['AdresseE']; ?>
+                                <div class="col-sm-10">
+                                    <input type="text" readonly class="form-control-plaintext" name="AdresseE" value="<?php echo $row['AdresseE']; ?>">
+                                </div>
                             </TD>
                         </TR>
 
@@ -90,7 +98,9 @@ session_start();
                                 <label for="nom">Tel :</label>
                             </TD>
                             <TD>
-                                <?php echo $row['TelE']; ?>
+                                <div class="col-sm-10">
+                                    <input type="text" readonly class="form-control-plaintext" name="TelE" value="<?php echo $row['TelE']; ?>">
+                                </div>
                             </TD>
                         </TR>
 
@@ -99,7 +109,9 @@ session_start();
                                 <label for="priorite">Vous êtes :</label>
                             </TD>
                             <TD>
-                                <?php echo $row['Statut']; ?>
+                                <div class="col-sm-10">
+                                    <input type="text" readonly class="form-control-plaintext" name="Statut" value="<?php echo $row['Statut']; ?>">
+                                </div>
                             </TD>
                         </TR>
 
@@ -108,10 +120,12 @@ session_start();
                                 <label for=" priorite ">Formation:</label>
                             </TD>
                             <TD>
-                                <?php echo $row['Formation']; ?>
+                                <div class="col-sm-10">
+                                    <input type="text" readonly class="form-control-plaintext" name="Formation" value="<?php echo $row['Formation']; ?>">
+                                </div>
                             </TD>
                             <TD>
-                                <label for=" priorite "> Ou Secretariat:</label>
+                                <label for=" priorite ">Ou Secretariat :</label>
                             </TD>
                             <TD>
                                 <SELECT id=" priorite " name=" priorite " size=" 1 ">
