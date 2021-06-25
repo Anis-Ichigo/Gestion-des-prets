95f6af60b57aa740d1cbea2f1ae068ad88177c23
MarineCbl <cabirol.marine@orange.fr>
2021-06-25 22:32:51 +0200

    Update entretien.php

--- entretien.php
+++ entretien.php
@@ -189,7 +189,7 @@ date_default_timezone_set('Europe/Paris');
     }
   }
   ?>
-  <div class="modal fade" id="succes_info" tabindex="-1" aria-hidden="true">
+  <div class="modal fade" id="succes_ajouter" tabindex="-1" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered">
           <div class="modal-content">
               <div class="modal-body">
@@ -199,7 +199,7 @@ date_default_timezone_set('Europe/Paris');
                       </svg>
 
                       <div>
-                          <?php echo "Le matériel a été ajouté avec sucess"; ?>
+                          <?php echo "Le matériel a été ajouté avec succès"; ?>
                       </div>
                   </div>
               </div>
@@ -214,7 +214,7 @@ date_default_timezone_set('Europe/Paris');
   </div>
   <script>
       $(window).load(function() {
-          $('#succes_info').modal('show');
+          $('#succes_ajouter').modal('show');
       });
   </script>
 
@@ -455,6 +455,34 @@ date_default_timezone_set('Europe/Paris');
   }
   ?>
 
+  <div class="modal fade" id="succes_supprimer" tabindex="-1" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+              <div class="modal-body">
+                  <div class="alert alert-success d-flex align-items-center" role="alert">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
+                          <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                      </svg>
+
+                      <div>
+                          <?php echo "Le matériel a été supprimé avec succès"; ?>
+                      </div>
+                  </div>
+              </div>
+
+              <div class="modal-footer">
+                  <div class="col text-center">
+                      <input type="button" class="btn btn-primary" data-bs-dismiss="modal" value="<?php echo TXT_OK; ?>">
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+  <script>
+      $(window).load(function() {
+          $('#succes_supprimer').modal('show');
+      });
+  </script>
 
   <script>
     function nouvelleCategorie() {
