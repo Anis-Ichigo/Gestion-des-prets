1db7b69ae63d106b8f0bfe7b601551ca3c10ffd0
haoyang314 <haoyang0314@gmail.com>
2021-06-30 10:14:03 +0200

    PDF

--- pdf.php
+++ pdf.php
@@ -5,6 +5,36 @@ mysqli_set_charset($session, "utf8");
 date_default_timezone_set('Europe/Paris');
 
 require('fpdf183/fpdf.php');
+
+$IdentifiantE = $_POST['IdentifiantE'];
+
+
+$informations = "SELECT materiel.IdentifiantM AS 'IdentifiantM', materiel.CategorieM AS 'CategorieM', emprunt.DateRetour AS 'DateRetour', modele.IdentifiantMo AS 'IdentifiantMo', modele.Marque AS 'Marque', emprunt.DateEmprunt AS 'DateEmprunt', emprunt.IdentifiantE AS 'IdentifiantE', personne.PrenomPe AS 'PrenomPe', personne.NomPe AS 'NomPe'
+                                        FROM personne, materiel, emprunt, modele
+                                        WHERE emprunt.IdentifiantM = materiel.IdentifiantM
+                                        AND emprunt.IdentifiantPe = personne.IdentifiantPe
+                                        AND materiel.IdentifiantMo = modele.IdentifiantMo
+                                        AND emprunt.IdentifiantE = '$IdentifiantE'";
+$result = mysqli_query($session, $informations);
+
+foreach ($result as $row) {
+    $IdentifiantM = $row['IdentifiantM'];
+    $CategorieM = $row['CategorieM'];
+    $date_retour = strftime('%d/%m/%Y', strtotime($row['DateRetour']));;
+    $modele = $row['IdentifiantMo'];
+    $marque = $row['Marque'];
+    $date_emprunt = strftime('%d/%m/%Y', strtotime($row['DateEmprunt']));
+    $IdentifiantE = $row['IdentifiantE'];
+    $prenom = $row['PrenomPe'];
+    $nom = $row['NomPe'];
+}
+
+
+if ($CategorieM == 'Ordinateur') {
+    $var = "un";
+} else {
+    $var = "une";
+}
 class PDF extends FPDF
 {
     protected $B = 0;
@@ -86,7 +116,7 @@ class PDF extends FPDF
     }
 }
 
-
+/*
 $prenom = 'haoyang';
 $nom = 'yu';
 $var = 'une';
@@ -96,33 +126,35 @@ $date_retour = '21/07/2021';
 $modele = 'Inspiron 15 3000';
 $marque = 'DELL';
 $date_emprunt = '21/06/2021';
+*/
 
 $pdf = new PDF();
 $pdf->SetLeftMargin(30);
-$pdf->SetRightMargin(15);
+$pdf->SetRightMargin(30);
 $pdf->AddPage();
 $pdf->Image('miage.jpg', 10, 6, 30);
 $pdf->Ln(60);
 $pdf->SetFont('Helvetica', '', 14);
 
-$pdf->WriteHTML(iconv('UTF-8', 'windows-1252', "Je soussigné(e) <b>{$prenom} {$nom} </b>, déclare recevoir {$var} <b>{$CategorieM} N°{$IdentifiantM}.</b>Je m’engage à restituer le matériel à tout moment si le responsable de la formation en a besoin ou avant le <b>{$date_retour}</b> dans le pire des cas.
+$pdf->WriteHTML(iconv('UTF-8', 'windows-1252', "Je soussigné(e) <b>{$prenom} {$nom} </b>, déclare recevoir {$var} <b>{$CategorieM} N°{$IdentifiantM}.</b> Je m’engage à restituer le matériel à tout moment si le responsable de la formation en a besoin ou avant le <b>{$date_retour}</b> dans le pire des cas.
 "));
 $pdf->Ln(15);
-$pdf->WriteHTML(iconv('UTF-8', 'windows-1252', "Le prêt comprend :<br>{$var} <b>{$CategorieM} {$modele}</b> de la marque <b>{$marque}</b> et une sacoche."));
-
+$pdf->WriteHTML(iconv('UTF-8', 'windows-1252', "Le prêt comprend :<br>- {$var} <b>{$CategorieM} {$modele}</b> de la marque <b>{$marque}</b>"));
+$pdf->Ln();
+$pdf->WriteHTML(iconv('UTF-8', 'windows-1252',"- une sacoche"));
 
 $pdf->Ln(40);
-$pdf->SetLeftMargin(145);
+$pdf->SetLeftMargin(135);
 $pdf->MultiCell(0, 10, iconv('UTF-8', 'windows-1252', "Fait le {$date_emprunt}"));
 
 $pdf->SetLeftMargin(30);
 $pdf->SetRightMargin(15);
 $pdf->Ln(15);
-$pdf->Image('box.png', 31, 192, 5, 0, '');
+$pdf->Image('box.png', 31, 202, 5, 0, '');
 $pdf->SetLeftMargin(40);
 $pdf->WriteHTML(iconv('UTF-8', 'windows-1252', "Je certifie sur l'honneur être d'accord avec le présent contrat."));
 $pdf->Ln(15);
-$pdf->Image('box.png', 31, 206, 5, 0, '');
+$pdf->Image('box.png', 31, 217, 5, 0, '');
 $pdf->SetLeftMargin(40);
 $pdf->WriteHTML(iconv('UTF-8', 'windows-1252', "En cochant cette case, je consent à l'utilisation de ma signature électronique, je certifie qu'elle est valide et a le même effet qu'une signature écrite sur une copie papier de ce document."));
 
